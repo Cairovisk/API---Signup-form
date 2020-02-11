337a1b658362fe997f31017b753dad798713ba60
Cairo Hoffmann <cairohoffmann@MacBook-Air-de-Cairo.local>
2020-02-11 16:57:23 -0300

    atualizando

--- app.js
+++ app.js
@@ -13,13 +13,34 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
+
+
     var firstName = req.body.fName;
     var lastName = req.body.lName;
     var email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    var options = {
+        url: "https://us4.api.mailchimp.com/3.0/lists/503d9a8adc",
+        method: "POST",
+
+    };
+    
+    request(options, function(error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(response.statusCode);
+        }
+    });
+    
 });
+
+
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000."); 
 });
+
+// 217a6e5a12dbef9b01bf41f6b96a7e30-us4
+
+// api id request 503d9a8adc
